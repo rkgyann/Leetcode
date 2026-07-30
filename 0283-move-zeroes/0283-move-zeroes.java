01ecdac489205c4217391c9e928f1d952d650518
Gyanendra Singh <193624677+rkgyann@users.noreply.github.com>
2026-07-30 22:51:26 +0530

    Time: 1 ms (99.88%), Space: 47.8 MB (57.44%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int write = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                nums[write++] = num;
+            }
+        }
+
+        while (write < nums.length) {
+            nums[write++] = 0;
+        }
+    }
+}
