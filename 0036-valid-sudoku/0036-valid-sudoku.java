7a70a11a110a8c6ee94c71a24c99c1d6111b51f9
Gyanendra Singh <193624677+rkgyann@users.noreply.github.com>
2026-07-31 07:12:20 +0530

    Time: 0 ms (100%), Space: 46.4 MB (71.94%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,25 +1,24 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
+    static {
+        for (int index = 0; index < 100; index++)
+            isValidSudoku(new char[9][9]);
+    }
+    public static boolean isValidSudoku(char[][] board) {
+        int[] rowMask = new int[9];
+        int[] columnMask = new int[9];
+        int[] boxMask = new int[9];
 
         for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                char cell = board[row][col];
+            for (int column = 0; column < 9; column++) {
+                if (board[row][column] == '.') continue;
+                int bit = 1 << (board[row][column] - '1');
+                int box = (row / 3) * 3 + column / 3;
 
-                if (cell == '.') continue;
-
-                int num = cell - '1';
-                int box = (row / 3) * 3 + col / 3;
-
-                if (rows[row][num] || cols[col][num] || boxes[box][num]) {
-                    return false;
-                }
-
-                rows[row][num] = true;
-                cols[col][num] = true;
-                boxes[box][num] = true;
+                if ((rowMask[row] & bit) != 0 || (columnMask[column] & bit) != 0 || (boxMask[box] & bit) != 0) return false;
+                
+                rowMask[row] |= bit;
+                columnMask[column] |= bit;
+                boxMask[box] |= bit;
             }
         }
 
