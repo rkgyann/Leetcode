53217dd729c38e3ec732458ccd7bf9205b644147
Gyanendra Singh <193624677+rkgyann@users.noreply.github.com>
2026-08-27 00:09:28 +0530

    Time: 2 ms (13.59%), Space: 47.1 MB (11.08%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        int i;
+
+        String[] answer = new String[n];
+        for (i = 0; i < n; i++) {
+            answer[i] = String.valueOf(i + 1);
+        }
+
+        if (n>=3) {
+            for (i = 0; i < n; i++) {
+                if ((i + 1) % 5 == 0)
+                    answer[i] = "Buzz";
+                if ((i + 1) % 3 == 0)
+                    answer[i] = "Fizz";
+            }
+        }
+
+        if (n>=15) {
+            for (i = 0; i < n; i++) {
+                if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0)
+                    answer[i] = "FizzBuzz";
+            }
+        }
+        return List.of(answer);
+    }
+}
