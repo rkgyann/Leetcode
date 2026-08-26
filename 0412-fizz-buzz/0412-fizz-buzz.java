b49d5f13259598e2631620311382bb84fba3929a
Gyanendra Singh <193624677+rkgyann@users.noreply.github.com>
2026-08-27 00:11:28 +0530

    Time: 4 ms (1.91%), Space: 46.7 MB (83.9%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -7,21 +7,19 @@ class Solution {
             answer[i] = String.valueOf(i + 1);
         }
 
-        if (n>=3) {
-            for (i = 0; i < n; i++) {
+        for (i = 0; i < n; i++) {
+            if (n >= 3) {
                 if ((i + 1) % 5 == 0)
                     answer[i] = "Buzz";
                 if ((i + 1) % 3 == 0)
                     answer[i] = "Fizz";
             }
-        }
-
-        if (n>=15) {
-            for (i = 0; i < n; i++) {
+            if (n >= 15) {
                 if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0)
                     answer[i] = "FizzBuzz";
             }
         }
+
         return List.of(answer);
     }
 }
